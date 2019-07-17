6d858fb7e134d98ece50bc29287a40d004931ea4
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-17 18:39:46 +0000

    Implemented Form Validation for null values and tweets more than 140 chars

--- public/scripts/postTweet.js
+++ public/scripts/postTweet.js
@@ -2,9 +2,18 @@
 $(document).ready(function() {
   $('form').submit(function(event) {
     const $form = $(this);
+
+    const inputText = $form.find('textarea[name="text"]').val();
+
+    if (!inputText) {
+      alert("Tweet can't be empty");
+    } else if (inputText.length > 140) {
+      alert("Tweet can't be more than 140 characters");
+    } else {
+      $.post($form.attr('action'), $form.serialize())
+        .fail(error => console.error(error));
+    }
     
-    $.post($form.attr('action'), $form.serialize())
-      .fail(error => console.error(error));
     event.preventDefault();
   });
 });
