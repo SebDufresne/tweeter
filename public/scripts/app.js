7130cd98c2a872df27e126ad2c9dc9d2f0802ee9
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-17 02:30:20 +0000

    Created renderTweets function to go through tweet list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,18 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(tweet) {
   const $tweet = $('<article>').addClass('tweet');
@@ -49,8 +62,8 @@ const createTweetElement = function(tweet) {
   const $timeP = $('<p>');
 
   $('<time>')
-    .attr('datetime', tweetData.created_at)
-    .text(tweetData.created_at)
+    .attr('datetime', tweet.created_at)
+    .text(tweet.created_at)
     .appendTo($timeP);
 
   $footer.append($timeP);
@@ -76,7 +89,10 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+};
 
-// Test / driver code (temporary)
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
