39c297dae86524526fde2754b076b9d833913588
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-17 18:24:53 +0000

    Created function loadTweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 /*
 <article class="tweet">
   <header>
@@ -140,4 +115,10 @@ const renderTweets = function(tweets) {
   }
 };
 
-renderTweets(data);
+const loadTweets = () => {
+  $.get('/tweets')
+    .then(tweets => renderTweets(tweets))
+    .fail(error => console.error(error));
+};
+
+loadTweets();
