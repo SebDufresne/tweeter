452ed1940105d0822162b171bfc0841ee9dcf17d
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-17 18:46:08 +0000

    Fixed but in submit new tweet

--- public/scripts/postTweet.js
+++ public/scripts/postTweet.js
@@ -3,17 +3,18 @@ $(document).ready(function() {
   $('form').submit(function(event) {
     const $form = $(this);
 
-    const inputText = $form.find('textarea[name="text"]').val();
+    const $inputText = $form.find('textarea[name="text"]');
 
-    if (!inputText) {
+    if (!$inputText.val()) {
       alert("Tweet can't be empty");
-    } else if (inputText.length > 140) {
+    } else if ($inputText.val().length > 140) {
       alert("Tweet can't be more than 140 characters");
     } else {
       $.post($form.attr('action'), $form.serialize())
         .fail(error => console.error(error));
+      $inputText.val('');
     }
-    
+
     event.preventDefault();
   });
 });
